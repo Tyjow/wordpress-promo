c1713e4f7a5b63fbe6ba7c5703f52a9a46e5bffe
Megaju <julien_malle@outlook.fr>
2016-10-03 16:32:41 +0200

    Style header partie 1 + orga style.css

--- wp-content/themes/site-promo-rennes/index.php
+++ wp-content/themes/site-promo-rennes/index.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
    
     <div class="page">
+       
+       
+        <div id="header" style='background-image:url(<?php header_image(); ?>)'></div>   
+       
         <!-- aside -->
         <?php get_sidebar(); ?>
             <div class="content">
