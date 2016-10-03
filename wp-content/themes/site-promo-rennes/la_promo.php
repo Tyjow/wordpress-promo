6649ea3daceabc8dd6e182c75ea64fdcb8d51aa7
nabil face <yampolos@gmail.com>
2016-10-03 17:10:23 +0200

    soucis bootstrap

--- wp-content/themes/site-promo-rennes/la_promo.php
+++ wp-content/themes/site-promo-rennes/la_promo.php
@@ -5,36 +5,36 @@
 ?>
 <div class="row">
 
-	<div class="front col-lg-6 col-md-6 col-sm-6 col-xs-6">
+		<div class="front col-lg-6 col-md-6 col-sm-6 col-xs-6">
 
-		<div class="apprenant">
-			<img src="" alt="">
-			<button class="">CV</button>
-			<button class="">Portfolio</button>
+			<div class="apprenant col-lg-6 col-md-6 col-sm-12 col-xs-12">
+				<img src="" alt="">
+				<button class="">CV</button>
+				<button class="">Portfolio</button>
+			</div>
+
+			<div class="apprenant col-lg-6 col-md-6 col-sm-12 col-xs-12">
+				<img src="" alt="">
+				<button class="">CV</button>
+				<button class="">Portfolio</button>
+			</div>
+			
 		</div>
 
-		<div class="apprenant">
-			<img src="" alt="">
-			<button class="">CV</button>
-			<button class="">Portfolio</button>
+		<div class="back col-lg-6 col-md-6 col-sm-6 col-xs-6">
+
+			<div class="apprenant col-lg-6 col-md-6 col-sm-12 col-xs-12">
+				<img src="" alt="">
+				<button class="">CV</button>
+				<button class="">Portfolio</button>
+			</div>
+
+			<div class="apprenant col-lg-6 col-md-6 col-sm-12 col-xs-12">
+				<img src="" alt="">
+				<button class="">CV</button>
+				<button class="">Portfolio</button>
+			</div>
+			
 		</div>
 		
-	</div>
-
-	<div class="back col-lg-6 col-md-6 col-sm-6 col-xs-6">
-
-		<div class="apprenant">
-			<img src="" alt="">
-			<button class="">CV</button>
-			<button class="">Portfolio</button>
-		</div>
-
-		<div class="apprenant">
-			<img src="" alt="">
-			<button class="">CV</button>
-			<button class="">Portfolio</button>
-		</div>
-		
-	</div>
-	
 </div>
