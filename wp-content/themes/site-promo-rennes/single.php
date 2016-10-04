5e57cee6212589cd699f8b89999d1fc64c30271d
Tyjow <platon.jonathan@gmail.com>
2016-10-04 15:13:19 +0200

    updated sidebar 0.2.1

--- wp-content/themes/site-promo-rennes/single.php
+++ wp-content/themes/site-promo-rennes/single.php
@@ -4,12 +4,12 @@
     <h2><?php the_title(); ?></h2>
 </div>
 <div class="fond-single">
-   <section class="single-article flex-sidebar">
+   <section class="single-article">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>                            
 
             <!-- bloc de l'article -->
             <div><?php the_post_thumbnail(array(678,0));?></div>
-            <div class="content-single flex-sidebar">
+            <div class="content-single">
                 <p><?php the_content(); ?></p>
                 <a href="http://localhost/wordpress-promo/"><button>Retour à l'accueil</button></a>
             </div>   
