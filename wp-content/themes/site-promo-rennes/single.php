70f823d3a3c675f8b1bc949b6fe3e6eda6b4e7cd
Tyjow <platon.jonathan@gmail.com>
2016-10-04 16:33:27 +0200

    fix newsletter sidebar

--- wp-content/themes/site-promo-rennes/single.php
+++ wp-content/themes/site-promo-rennes/single.php
@@ -11,7 +11,7 @@
             <div><?php the_post_thumbnail(array(678,0));?></div>
             <div class="content-single">
                 <p><?php the_content(); ?></p>
-                <a href="http://localhost/wordpress-promo/"><button>Retour à l'accueil</button></a>
+                <a class="button-news" href="http://localhost/wordpress-promo/">Retour à l'accueil</a>
             </div>   
                 
     </section>
