768684d072ac608cda25d6e0d477437282a686fc
nabil face <yampolos@gmail.com>
2016-10-04 14:19:51 +0200

    page list apprenant en cours/partage de bg.png

--- wp-content/themes/site-promo-rennes/la_promo.php
+++ wp-content/themes/site-promo-rennes/la_promo.php
@@ -4,38 +4,56 @@
 */
 ?>
 <?php get_header(); ?>
-<div class="row">
+<div class="listApprenant row">
 
-		<div class="front col-lg-6 col-md-6 col-sm-6 col-xs-6">
+	<div class="front col-lg-6 col-md-6 col-sm-6 col-xs-6">
 
-			<div class="apprenant col-lg-6 col-md-6 col-sm-6 col-xs-12">
+		<h2 class="frontTitre">Front-end</h2>
+
+			<div class="apprenant col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-offset-2 col-sm-8 col-lg-offset-2 col-xs-8">
 				<img class="img-responsive" src="" alt="">
-				<button class="">CV</button>
-				<button class="">Portfolio</button>
+				<!-- hidden info box-->
+				<div class="infoBox">
+					<h4></h4>
+					<p></p>
+					<div class="progress">
+						<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">HTML5/CSS3</div>
+						<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">JS</div>
+						<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%"></div>
+					</div>
+				</div>
+				<h3 class="nomApprenant">Jean-Lorem Ipsum</h3>
+				<a href="" class="btnCv">CV</a>
+				<a href="" class="btnFolio">PORTFOLIO</a>
 			</div>
 
-			<div class="apprenant col-lg-6 col-md-6 col-sm-6 col-xs-12">
-				<img class="img-responsive" class="img-responsive" class="img-responsive" src="" alt="">
-				<button class="">CV</button>
-				<button class="">Portfolio</button>
+			<div class="apprenant col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-offset-2 col-sm-8 col-lg-offset-2 col-xs-8">
+				<img class="img-responsive" src="" alt="">
+				<h3 class="nomApprenant">Marie-Lorem Ipsum</h3>
+				<a href="" class="btnCv">CV</a>
+				<a href="" class="btnFolio">PORTFOLIO</a>
 			</div>
 			
-		</div>
+	</div>
 
-		<div class="back col-lg-6 col-md-6 col-sm-6 col-xs-6">
+	<div class="back col-lg-6 col-md-6 col-sm-6 col-xs-6">
 
-			<div class="apprenant col-lg-6 col-md-6 col-sm-6 col-xs-12">
-				<img class="img-responsive" class="img-responsive" src="" alt="">
-				<button class="">CV</button>
-				<button class="">Portfolio</button>
+		<h2 class="backTitre">Back-end</h2>
+
+			<div class="apprenant col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-offset-2 col-sm-8 col-lg-offset-2 col-xs-8">
+				<img class="img-responsive" src="" alt="">
+				<h3 class="nomApprenant">Aït Lorem Ben Ipsum</h3>
+				<a href="" class="btnCv">CV</a>
+				<a href="" class="btnFolio">PORTFOLIO</a>
 			</div>
 
-			<div class="apprenant col-lg-6 col-md-6 col-sm-6 col-xs-12">
+			<div class="apprenant col-lg-offset-1 col-lg-5 col-md-offset-1 col-md-5 col-sm-offset-2 col-sm-8 col-lg-offset-2 col-xs-8">
 				<img class="img-responsive" src="" alt="">
-				<button class="">CV</button>
-				<button class="">Portfolio</button>
+				<h3 class="nomApprenant">Lorem Ben Ipsum</h3>
+				<a href="" class="btnCv">CV</a>
+				<a href="" class="btnFolio">PORTFOLIO</a>
 			</div>
 			
-		</div>
+	</div>
 		
 </div>
