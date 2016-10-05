5a91175363f54c9e86e4a1a0adb07840185028e7
Tyjow <platon.jonathan@gmail.com>
2016-10-05 11:45:27 +0200

    updated single page

--- wp-content/themes/site-promo-rennes/single.php
+++ wp-content/themes/site-promo-rennes/single.php
@@ -1,17 +1,15 @@
 <?php get_header(); ?>
 
-<div class="bandeau bandeau-mt0 text-center">
-    <h2><?php the_title(); ?></h2>
-</div>
 <div class="fond-single">
    <section class="single-article">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>                            
-
             <!-- bloc de l'article -->
             <div><?php the_post_thumbnail(array(678,0));?></div>
             <div class="content-single">
+               <h2 class="article-title"><?php the_title(); ?></h2>
                 <p><?php the_content(); ?></p>
-                <a class="button-news" href="http://localhost/wordpress-promo/index.php">Retour à l'accueil</a>
+
+                <a class="button-news" href="http://localhost/wordpress-promo/">Retour aux news</a>
             </div>   
                 
     </section>
@@ -20,7 +18,6 @@
     <?php endwhile; else: ?>
         <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
     <?php endif; ?>            
-</div>
                     
 <?php get_footer(); ?>
 
