19f800a4b92ce237366536e0a5fcc7b5b637e2bc
Anna-AP <paestum1@gmail.com>
2016-10-05 14:55:31 +0200

    fin de mise en page du formulaire de contact, reste le bug d'envoi à résoudre

--- wp-content/themes/site-promo-rennes/contact.php
+++ wp-content/themes/site-promo-rennes/contact.php
@@ -4,6 +4,10 @@
     */
 ?>
 
+<?php get_header(); ?>
+<div class="formulaire_de_contact">
+    <?php echo do_shortcode('[contact-form-7 id="4" title="Contact form 1"]');?>
+</div>
 <?php 
     the_post(); the_content();
 ?>
