912bfd140b7685cd94ac3a0c3cd7c2c09145bef3
erwannFace <e.duclos@fondationface.org>
2016-10-24 13:43:31 +0200

    Updating Code Academie presentation

--- wp-content/themes/site-promo-rennes/index.php
+++ wp-content/themes/site-promo-rennes/index.php
@@ -28,21 +28,30 @@
                         <h2>A PROPOS</h2>
                         <img src="<?php echo get_template_directory_uri(); ?>/img/logo-codeacademie.png"/>
                     </div>
-                    <p>La Code Académie, une formation qui permet l'apprentissage du développement avec différents langages de programmation informatique web/mobiles pour tous les profils qui démontrent une fortes motivation à apprendre la programmation.
-Le nom de notre formation Code Académie, formation proposée par la fondation  <a href="http://face.bzh/">FACE </a> qui a répondu à l'appel d'offre afin d’être labellisé a la <a href="https://www.grandeecolenumerique.fr/"> Grande école du numérique </a> et dont <a href="http://simplon.co/"> Simplon</a> est le prestataire de la pédagogie.
-La Code Académie est conventionnée et subventionnée par la Région Bretagne et L’état.La formation se déroule au siège social de la <a href="http://face.bzh/">Fondation pour Agir Contre l'Exclusion.</a>
-Elle a débuté en mai et se termine fin janvier.</p>
+                    <p>La Code Académie est un dispositif mis en place dans le cadre de la  <a href="https://www.grandeecolenumerique.fr/"> Grande école du numérique</a>.
+Il s'agit d'une formation qui permet l'apprentissage du développement web/mobile pour tous les profils qui démontrent une forte motivation à apprendre des langages de programmation.
+ Cette formation, proposée par la fondation <a href="http://face.bzh/">FACE</a>, suit la pédagogie <a href="http://simplon.co/">Simplon</a>; soit la méthode du "Learning by doing".
+ La Code Académie est conventionnée et subventionnée par la Région Bretagne et l’Etat.
+ La première promotion de la formation, qui se déroule à la <a href="http://face.bzh/">Fondation pour Agir Contre l'Exclusion</a>, a débuté le 17 mai et se termine fin janvier.</p>
                 </article>
                 
                 <article class="center">
                     <h3>La formation</h3>
-                    <p>Spécialement orienté vers les travaux pratiques, cette fromation permet la réalisation de nombreux projets professionnels, en relation direct avec des entreprises. </br>
-                ICI lien de 1/2/3veto -fedala -ebulition </br>
-Ainsi l'apprentissage des bases du développement web, avec des languages tels que HTML5/CSS3 et Javascript se deroule avec des réeles contraintes techniques en lien avec les demandes de clients.</br>
-La réalisation de ces projets proffessionnels, permet une formation axée sur le travail en équipe et donc de les familiariser avec differentes méthode de travail, Dont la méthode Agile, notamment en mode SCRUM.(scrum sera un lien vers un article sur ce que c'est) </br>
-Mais aussi d'experimenter le développement collaboratif avec des outils tel que <a href="https://fr.wikipedia.org/wiki/Git">Git/GitHub.</a> </br>
-La seconde partie de formation est axée sur une spécialisation, ainsi chaque apprenant a la possibilité d'approfondir ses connaissances dans son secteur.</br>
-De nombreuses interventions d'acteurs du secteur numérique leur permettent d'acquerir une vision global du milieu numérique sur le bassin Rennais. </p>
+<p>
+Spécialement orientée vers les travaux pratiques, cette formation permet la réalisation de nombreux projets professionnels, en relation directe avec des entreprises. 
+
+Ainsi l'apprentissage des bases du développement web,et des langages tels que HTML5/CSS3, Javascript et PHP se déroule avec de réelles contraintes techniques en lien avec les demandes des clients.
+La réalisation de ces projets professionnels, favorise le travail en équipe et permet  donc, d’une part,  de familiariser les apprenant.e.s à  différentes méthodes de travail, dont le <em>pair programming</p>em> ou la méthode <em>Agile</em>, notamment en mode <em>SCRUM</em>.
+Mais Cela leur permet donne d’autre part aussi  l’opportunité d'expérimenter le développement collaboratif avec des outils tels que Git/GitHub. 
+
+La seconde partie de formation est axée sur une spécialisation, ainsi chaque apprenant.e a la possibilité d'approfondir ses connaissances dans son secteur, que ce soit :
+<ul>
+<li>une spécialisation Front End, dans laquelle ils.elles découvrent des frameworks tels que AngularJS ou REACT</li>
+ <li>une spécialisation Back End où ils.elles pratiquent le framework <em>Silex</em>, et <em>Symfony</em> et NodeJS</li>
+ <li>une spécialisation Référent Numérique où les apprenant.e.s approfondissent le CMS Wordpress et les notions de gestion de projet.</li>
+ </ul>
+Enfin, tout au long du parcours, de nombreuses interventions d'acteurs du secteur numérique leurs permettent d'acquérir une vision globale du milieu numérique sur le bassin Rennais.
+</p>
                 </article>
             </div>
             
