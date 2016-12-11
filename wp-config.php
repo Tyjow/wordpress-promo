12e2ae2bee9ad86e2594a66ea1159ba063cad736
Erwann DUCLOS <educlos@simplon.co>
2016-12-11 08:04:29 +0100

    Cleaning code

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'sitePromo');
+define('DB_NAME', 'site-promo');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'trainer');
+define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'facesimplon2016');
+define('DB_PASSWORD', 'facesimplon');
 
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
