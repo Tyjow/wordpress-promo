1b417c0f3790ea7180c1a55df1d1b5e701cc60a4
[Massacor] <[apowarp@gmail.com]>
2017-01-04 10:44:52 +0100

    added portfolios to gitignore

--- wp-content/themes/site-promo-rennes/header.php
+++ wp-content/themes/site-promo-rennes/header.php
@@ -1,6 +1,6 @@
 <?php //123veto ?>
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
+    <html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
 
     <head profile="http://gmpg.org/xfn/11">
         <title>
